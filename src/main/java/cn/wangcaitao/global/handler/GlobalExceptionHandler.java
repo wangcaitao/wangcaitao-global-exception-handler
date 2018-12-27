38f6d9597c49e9a790f7c8d2ec80831b1a4f91fb
wangcaitao <wangcaitao@aliyun.com>
2018-12-27 19:42:25 +0800

    增加异常捕获处理

--- src/main/java/cn/wangcaitao/global/handler/GlobalExceptionHandler.java
+++ src/main/java/cn/wangcaitao/global/handler/GlobalExceptionHandler.java
@@ -4,10 +4,13 @@ import cn.wangcaitao.common.constant.ResultConstant;
 import cn.wangcaitao.common.entity.Result;
 import cn.wangcaitao.common.exception.ResultException;
 import cn.wangcaitao.common.util.ResultUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -37,9 +40,18 @@ public class GlobalExceptionHandler {
         } else if (e instanceof HttpRequestMethodNotSupportedException) {
             return ResultUtils.error(ResultConstant.METHOD_NOT_ALLOWED_CODE, ResultConstant.METHOD_NOT_ALLOWED_MSG);
         } else if (e instanceof ResultException) {
+            String params = ((ResultException) e).getParams();
+            if (StringUtils.isNotEmpty(params)) {
+                LOGGER.error("request params: {}", params, e);
+            }
+
             return ResultUtils.error(((ResultException) e).getCode(), ((ResultException) e).getMsg());
+        } else if (e instanceof HttpMessageNotReadableException) {
+            return ResultUtils.error(ResultConstant.BAD_REQUEST_CODE, ResultConstant.BAD_REQUEST_MSG);
+        } else if (e instanceof HttpMediaTypeNotSupportedException) {
+            return ResultUtils.error(ResultConstant.UNSUPPORTED_MEDIA_TYPE_CODE, ResultConstant.UNSUPPORTED_MEDIA_TYPE_MSG);
         } else {
-            LOGGER.warn("un handle exception: {}", e.getClass().getName());
+            LOGGER.error("un handle exception: {}", e.getClass().getName());
 
             return ResultUtils.error(ResultConstant.INTERNAL_SERVER_ERROR_CODE, ResultConstant.INTERNAL_SERVER_ERROR_MSG);
         }
