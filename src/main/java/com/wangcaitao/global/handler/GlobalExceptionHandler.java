38d3b52b127cd3731d989b4f5f912ea188236202
wangcaitao <wangcaitao@aliyun.com>
2019-11-27 23:58:03 +0800

    格式修改

--- src/main/java/com/wangcaitao/global/handler/GlobalExceptionHandler.java
+++ src/main/java/com/wangcaitao/global/handler/GlobalExceptionHandler.java
@@ -1,68 +1,118 @@
 package com.wangcaitao.global.handler;
 
-import com.wangcaitao.common.constant.HttpStatusConstant;
+import com.wangcaitao.common.constant.HttpStatusEnum;
 import com.wangcaitao.common.entity.Result;
 import com.wangcaitao.common.exception.ResultException;
 import com.wangcaitao.common.util.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.validation.BindException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 /**
- * 统一异常处理
- *
  * @author wangcaitao
  */
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-
+    /**
+     * Exception handler
+     *
+     * @param e Exception
+     * @return Result
+     */
     @ExceptionHandler(value = Exception.class)
     public Result handlerException(Exception e) {
-        if (e instanceof NoHandlerFoundException) {
-            return ResultUtils.error(HttpStatusConstant.NOT_FOUND_CODE, HttpStatusConstant.NOT_FOUND_MSG);
-        } else if (e instanceof BindException) {
-            return ResultUtils.error(HttpStatusConstant.BAD_REQUEST_CODE, ((BindException) e).getFieldErrors().get(0).getDefaultMessage());
-        } else if (e instanceof MethodArgumentNotValidException) {
-            return ResultUtils.error(HttpStatusConstant.BAD_REQUEST_CODE, ((MethodArgumentNotValidException) e).getBindingResult().getFieldErrors().get(0).getDefaultMessage());
-        } else if (e instanceof AccessDeniedException) {
-            return ResultUtils.error(HttpStatusConstant.FORBIDDEN_CODE, HttpStatusConstant.FORBIDDEN_MSG);
-        } else if (e instanceof HttpRequestMethodNotSupportedException) {
-            return ResultUtils.error(HttpStatusConstant.METHOD_NOT_ALLOWED_CODE, HttpStatusConstant.METHOD_NOT_ALLOWED_MSG);
-        } else if (e instanceof ResultException) {
-            String params = ((ResultException) e).getParams();
-            if (StringUtils.isNotEmpty(params)) {
-                LOGGER.error("request params: {}", params, e);
-            }
+        log.error("un handler exception.", e);
 
-            return ResultUtils.error(((ResultException) e).getCode(), ((ResultException) e).getMsg());
-        } else if (e instanceof HttpMessageNotReadableException) {
-            return ResultUtils.error(HttpStatusConstant.BAD_REQUEST_CODE, null == e.getCause() ? "缺少请求参数" : e.getCause().getLocalizedMessage());
-        } else if (e instanceof HttpMediaTypeNotSupportedException) {
-            return ResultUtils.error(HttpStatusConstant.UNSUPPORTED_MEDIA_TYPE_CODE, HttpStatusConstant.UNSUPPORTED_MEDIA_TYPE_MSG);
-        } else if (e instanceof MethodArgumentTypeMismatchException) {
-            return ResultUtils.error(HttpStatusConstant.BAD_REQUEST_CODE, e.getMessage());
-        } else if (e instanceof ServletRequestBindingException) {
-            return ResultUtils.error(HttpStatusConstant.BAD_REQUEST_CODE, e.getMessage());
-        } else if (e instanceof HttpClientErrorException) {
-            return ResultUtils.error(HttpStatusConstant.BAD_REQUEST_CODE, "授权失败");
-        } else {
-            LOGGER.error("un handle exception: {}", e.getClass().getName(), e);
+        return ResultUtils.error();
+    }
 
-            return ResultUtils.error(HttpStatusConstant.INTERNAL_SERVER_ERROR_CODE, HttpStatusConstant.INTERNAL_SERVER_ERROR_MSG);
+    /**
+     * RuntimeException handler
+     *
+     * @param e Exception
+     * @return Result
+     */
+    @ExceptionHandler(value = RuntimeException.class)
+    public Result handlerException(RuntimeException e) {
+        log.error("runtime error.", e);
+
+        return ResultUtils.error();
+    }
+
+    /**
+     * NoHandlerFoundException handler
+     *
+     * @param e Exception
+     * @return Result
+     */
+    @ExceptionHandler(value = NoHandlerFoundException.class)
+    public Result handlerException(NoHandlerFoundException e) {
+        log.error("resource not fund.", e);
+
+        return ResultUtils.error(HttpStatusEnum.NOT_FOUND);
+    }
+
+    /**
+     * HttpMediaTypeNotSupportedException handler
+     *
+     * @param e HttpMediaTypeNotSupportedException
+     * @return Result
+     */
+    @ExceptionHandler(value = HttpMediaTypeNotSupportedException.class)
+    public Result handlerException(HttpMediaTypeNotSupportedException e) {
+        log.error("content-type error. content-type: {}", e.getContentType(), e);
+
+        return ResultUtils.error(415, "Content-Type 错误");
+    }
+
+    /**
+     * HttpMessageNotReadableException handler
+     *
+     * @param e HttpMessageNotReadableException
+     * @return Result
+     */
+    @ExceptionHandler(value = HttpMessageNotReadableException.class)
+    public Result handlerException(HttpMessageNotReadableException e) {
+        log.error("miss request body.", e);
+
+        return ResultUtils.error(400, "缺少请求参数");
+    }
+
+    /**
+     * HttpRequestMethodNotSupportedException handler
+     *
+     * @param e HttpRequestMethodNotSupportedException
+     * @return Result
+     */
+    @ExceptionHandler(value = HttpRequestMethodNotSupportedException.class)
+    public Result handlerException(HttpRequestMethodNotSupportedException e) {
+        log.error("method error. method: {}", e.getMethod(), e);
+
+        return ResultUtils.error(405, "请求方式错误");
+    }
+
+    /**
+     * ResultException handler
+     *
+     * @param e ResultException
+     * @return Result
+     */
+    @ExceptionHandler(value = ResultException.class)
+    public Result handlerException(ResultException e) {
+        log.error("result error. msg: {}", e.getMsg(), e);
+
+        String params = e.getParams();
+        if (StringUtils.isNotEmpty(params)) {
+            log.error("request params: {}", params, e);
         }
+
+        return ResultUtils.error(e.getCode(), e.getMsg());
     }
 }
